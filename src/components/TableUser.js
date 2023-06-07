95e93652fd0461791c3971849828ad4ad1728be1
aceblack0003 <aceblack0003@gmail.com>
2023-06-07 15:33:00 +0700

    Hiện Thị Danh Sách Users

---
+++ src/components/TableUser.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import { fetchAllUser } from '../services/UserService';
+
+const TableUsers = (props) => {
+
+    const [listUser, setListUser] = useState([])
+
+    useEffect(() => {
+        // call api
+
+        getUsers()
+    }, [])
+
+    const getUsers = async () => {
+        let res = await fetchAllUser();
+        if (res && res.data && res.data.data) {
+            setListUser(res.data.data)
+        }
+    }
+    return (
+        <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUser && listUser.length > 0 &&
+                        listUser.map((item, index) => {
+                            return (
+                                <tr key={`users-${index}`}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.first_name}</td>
+                                    <td>{item.last_name}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </Table>
+        </>
+    )
+}
+export default TableUsers
